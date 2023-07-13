303e5fbc20894daf9eb13904a9bf26878b8cce2c
Abigail Lang <abigaillang2772@gmail.com>
2023-07-13 11:26:20 -0700

    Initial Commit

---
+++ vstacks/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  vstacks
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image("santorini png")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Image("parthenon")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+        }
+        }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vstacks/vstacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  vstacksApp.swift
+//  vstacks
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct vstacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
